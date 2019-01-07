bfad3d63667e36ec6c7758dcb0ffa7e4ffcfc77b
Jacob Hughes <jacob.hughes@kcl.ac.uk>
2019-01-07 16:39:24 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
+
+# Compile to .ll LLVM-IR
 ./parser $1 2>&1 | sed -u 1,/BEGIN_IR/d > $2
+
+# Assemble IR
+llc $2 -o out.s
+
+# Compile and link with allocator
+clang++ -c allocator.cpp
+clang++ out.s allocator.o
+
+# run and hope
+./a.out
+
+
+
 
