061624cc3f3704241b11ba14ab9fc0c34ff72d6e
Jacob Hughes <jacob.hughes@kcl.ac.uk>
2019-01-21 19:49:32 +0000

    wip gc

--- runtime/allocator.cpp
+++ runtime/allocator.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 #include <stdlib.h>
+#include "includes/statepoint.h"
+
+#ifdef __linux__
+  #define STACKMAP __LLVM_StackMaps
+#endif
+
+extern uint8_t STACKMAP[];
+bool tableBuilt = false;
+statepoint_table_t* table;
+
 
 extern "C" {
 
@@ -13,5 +23,55 @@ extern "C" {
     void logger(long val) {
         std::cout << "Logger: " << val << std::endl;
     }
+
+    void genTable() {
+        void* stackmap = (void*)&STACKMAP;
+        if(!tableBuilt) {
+            printf("printing the table...\n");
+            table = generate_table(stackmap, 0.5);
+            print_table(stdout, table, true);
+            printf("\n\n");
+            // destroy_table(table);
+            tableBuilt = true;
+        }
+    }
+
+    void walkStack(uint8_t* stackPtr) {
+        std::cout << "Beginning GC" << std::endl;
+        uint64_t retAddr = *((uint64_t*)stackPtr);
+        stackPtr += sizeof(void*); // step into frame
+        frame_info_t* frame = lookup_return_address(table, retAddr);
+        while(frame != NULL) {
+
+            uint16_t i;
+            for(i = 0; i < frame->numSlots; i++) {
+                pointer_slot_t ptrSlot = frame->slots[i];
+                if(ptrSlot.kind >= 0) {
+                    // our example does not use derived pointers
+                    assert(false && "unexpected derived pointer\n");
+                }
+
+                uint32_t** ptr = (uint32_t**)(stackPtr + ptrSlot.offset);
+                std::cout << "Found Pointer: " << ptr << std::endl;
+            }
+
+            // move to next frame. seems we have to add one pointer size to
+            // reach the next return address? NOTE
+            stackPtr = stackPtr + frame->frameSize;
+
+            // grab return address of the frame
+            retAddr = *((uint64_t*)stackPtr);
+            stackPtr += sizeof(void*); // step into frame
+            frame = lookup_return_address(table, retAddr);
+
+        }
+
+    }
+
+    int main() {
+        genTable();
+        return 0;
+    }
+
 
 }
